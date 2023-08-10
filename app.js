289b2d819227b13e98f1c3996491bba96c086407
perttinen <samuli.viitala@gmail.com>
2023-08-10 18:53:20 +0300

    bkeaking build

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'))
+app.use(express.stati('dist'))
 
 app.get('/version', (req, res) => {
   res.send('7')
